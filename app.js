49047fb81ed639ccd6fda8eef36a0fa2a2e66590
Gaurang <gaurangjotwani@gmail.com>
2023-10-15 14:16:00 -0700

    cleaning app.js

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Add CORS headers to allow any origin
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  // You can also specify other CORS headers as needed
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
