b2ad990a95109535f013a6c29ace9832343ed776
Gaurang <gaurangjotwani@gmail.com>
2023-10-15 17:31:35 -0700

    Fix prod

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,6 @@
 let map;
 let infoWindow;
-let google;
+let markers = [];
 
 function initMap() {
   let sanMateo = { lat: 37.563, lng: -122.3255 };
@@ -12,60 +12,55 @@ function initMap() {
   infoWindow = new google.maps.InfoWindow();
 }
 
-function FrontEnd() {
-  let markers = [];
+const getStores = () => {
+  const zipCode = document.getElementById("zip-code").value;
+  if (!zipCode || zipCode.length != 5 || !isStringDigits(zipCode)) {
+    clearLocations();
+    InvalidZipCode();
+    return;
+  }
+  const API_URL = "https://coffeelocator.onrender.com/api/stores?";
+  fetch(
+    API_URL +
+      new URLSearchParams({
+        zip_code: zipCode,
+      })
+  )
+    .then((response) => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error("No stores found");
+      }
+    })
+    .then((data) => {
+      const { updatedCount, stores } = data;
 
-  const getStores = () => {
-    const zipCode = document.getElementById("zip-code").value;
-    if (!zipCode || zipCode.length != 5 || !isStringDigits(zipCode)) {
+      if (stores.length > 0) {
+        clearLocations();
+        searchLocationNear(stores);
+        setStoresList(stores);
+        setOnClickListener();
+
+        const updatedCountParagraph = document.getElementById("updated-count");
+        updatedCountParagraph.textContent =
+          "This has been searched for " + updatedCount + " times today! ";
+      } else {
+        clearLocations();
+        noStoresFound();
+      }
+    })
+    .catch((error) => {
       clearLocations();
       InvalidZipCode();
       return;
-    }
-    const API_URL = "https://coffeelocator.onrender.com/api/stores?";
-    fetch(
-      API_URL +
-        new URLSearchParams({
-          zip_code: zipCode,
-        })
-    )
-      .then((response) => {
-        if (response.status == 200) {
-          return response.json();
-        } else {
-          throw new Error("No stores found");
-        }
-      })
-      .then((data) => {
-        const { updatedCount, stores } = data;
+    });
+};
 
-        if (stores.length > 0) {
-          clearLocations();
-          searchLocationNear(stores);
-          setStoresList(stores);
-          setOnClickListener();
-
-          const updatedCountParagraph = document.getElementById(
-            "updated-count"
-          );
-          updatedCountParagraph.textContent =
-            "This has been searched for " + updatedCount + " times today! ";
-        } else {
-          clearLocations();
-          noStoresFound();
-        }
-      })
-      .catch(() => {
-        clearLocations();
-        InvalidZipCode();
-        return;
-      });
-  };
-
-  const setStoresList = (stores) => {
-    let storesHtml = ``;
-    stores.forEach((store, index) => {
-      storesHtml += `        
+const setStoresList = (stores) => {
+  let storesHtml = ``;
+  stores.forEach((store, index) => {
+    storesHtml += `        
       <div class="store-container">
           <div class="store-container-background">
             <div class="store-info-container">
@@ -80,52 +75,45 @@ function FrontEnd() {
             </div>
           </div>
         </div>`;
-    });
-    document.querySelector(".stores-list").innerHTML = storesHtml;
-  };
+  });
+  document.querySelector(".stores-list").innerHTML = storesHtml;
+};
 
-  const searchLocationNear = (stores) => {
-    let bounds = new google.maps.LatLngBounds();
-    stores.forEach((store, index) => {
-      var latlng = new google.maps.LatLng(
-        store.location.coordinates[1],
-        store.location.coordinates[0]
-      );
-      let name = store.storeName;
-      let address = store.addressLines[0];
-      let openStatusText = store.openStatusText;
-      let phoneNumber = store.phoneNumber;
-      bounds.extend(latlng);
-      createMarker(
-        latlng,
-        name,
-        address,
-        index + 1,
-        openStatusText,
-        phoneNumber
-      );
-    });
-    map.fitBounds(bounds);
-  };
+const searchLocationNear = (stores) => {
+  let bounds = new google.maps.LatLngBounds();
+  stores.forEach((store, index) => {
+    var latlng = new google.maps.LatLng(
+      store.location.coordinates[1],
+      store.location.coordinates[0]
+    );
+    let name = store.storeName;
+    let address = store.addressLines[0];
+    let openStatusText = store.openStatusText;
+    let phoneNumber = store.phoneNumber;
+    bounds.extend(latlng);
+    createMarker(latlng, name, address, index + 1, openStatusText, phoneNumber);
+  });
+  map.fitBounds(bounds);
+};
 
-  const setOnClickListener = () => {
-    let storeElements = document.querySelectorAll(".store-container");
-    storeElements.forEach((element, index) => {
-      element.addEventListener("click", () => {
-        google.maps.event.trigger(markers[index], "click");
-      });
+const setOnClickListener = () => {
+  let storeElements = document.querySelectorAll(".store-container");
+  storeElements.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      google.maps.event.trigger(markers[index], "click");
     });
-  };
+  });
+};
 
-  const createMarker = (
-    latlng,
-    name,
-    address,
-    storeNumber,
-    openStatusText,
-    phoneNumber
-  ) => {
-    let html = `
+const createMarker = (
+  latlng,
+  name,
+  address,
+  storeNumber,
+  openStatusText,
+  phoneNumber
+) => {
+  let html = `
     <div class="store-info-window">
       <div class="store-info-name">
         ${name}
@@ -151,52 +139,48 @@ function FrontEnd() {
       </div>
     </div>
   `;
-    let marker = new google.maps.Marker({
-      position: latlng,
-      map: map,
-      label: `${storeNumber}`,
-    });
-
-    google.maps.event.addListener(marker, "click", () => {
-      infoWindow.setContent(html);
-      infoWindow.open(map, marker);
-    });
-    markers.push(marker);
-  };
-
-  const clearLocations = () => {
-    infoWindow.close();
-    for (let i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-    markers.length = 0;
-  };
-
-  const inputElement = document.getElementById("zip-code");
-  inputElement.addEventListener("keyup", function (event) {
-    if (event.keyCode === "Enter") {
-      getStores();
-    }
+  let marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    label: `${storeNumber}`,
   });
 
-  const noStoresFound = () => {
-    const html = `<div class="no-stores-found">No Stores Found</div>`;
-    document.querySelector(".stores-list").innerHTML = html;
-  };
+  google.maps.event.addListener(marker, "click", () => {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+};
 
-  const InvalidZipCode = () => {
-    const html = `<div class="invalid-zip-code">Invalid Zip Code</div>`;
-    document.querySelector(".stores-list").innerHTML = html;
-  };
+const clearLocations = () => {
+  infoWindow.close();
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;
+};
 
-  const isStringDigits = (inputString) => {
-    for (let i = 0; i < inputString.length; i++) {
-      if (!/^\d$/.test(inputString[i])) {
-        return false;
-      }
+const onEnter = (e) => {
+  if (e.key == "Enter") {
+    getStores();
+  }
+};
+
+const noStoresFound = () => {
+  const html = `<div class="no-stores-found">No Stores Found</div>`;
+  document.querySelector(".stores-list").innerHTML = html;
+};
+
+const InvalidZipCode = () => {
+  const html = `<div class="invalid-zip-code">Invalid Zip Code</div>`;
+  document.querySelector(".stores-list").innerHTML = html;
+};
+
+const isStringDigits = (inputString) => {
+  for (let i = 0; i < inputString.length; i++) {
+    if (!/^\d$/.test(inputString[i])) {
+      return false;
     }
-    return true;
-  };
-}
-
-FrontEnd();
+  }
+  return true;
+};
